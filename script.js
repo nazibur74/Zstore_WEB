e4a1ca47845ef2c446ff9a17e62adfcbd9cf6b3e
nazibur74 <shafikhanzz72@gmail.com>
2024-12-14 23:22:37 +0600

    Updated README

--- script.js
+++ script.js
@@ -218,12 +218,6 @@ function validateRegister() {
     redirectToDashboard();
 }
 
-function validateEmail(email) {
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return emailPattern.test(email);
-}
-
 function redirectToDashboard() {
     window.location.href = "index.html";
 }
-
